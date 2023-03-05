d4a3578c75354395084386f61b55adc5b2b7f6c7
oumanatsumi <oumanatsumi@126.com>
2023-03-05 21:59:30 +0800

    实现用Ajax实时验证旧密码

--- src/main/java/com/oumanatsumi/servlet/user/UserServlet.java
+++ src/main/java/com/oumanatsumi/servlet/user/UserServlet.java
@@ -1,5 +1,6 @@
 package com.oumanatsumi.servlet.user;
 
+import com.alibaba.fastjson.JSONArray;
 import com.mysql.jdbc.StringUtils;
 import com.oumanatsumi.pojo.User;
 import com.oumanatsumi.service.user.UserService;
@@ -11,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServlet extends HttpServlet {
     @Override
@@ -26,9 +30,12 @@ public class UserServlet extends HttpServlet {
 
         if(method != null && method.equals("savepwd")){
             this.updatePwd(request, response);
+        }else if(method.equals("pwdmodify") && method!=null){
+            this.pwdModify(request,response);
         }
     }
 
+    // 修改密码
     public void updatePwd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 从Session中取ID
         Object o = request.getSession().getAttribute(Constants.USER_SESSION);
@@ -52,5 +59,38 @@ public class UserServlet extends HttpServlet {
             request.setAttribute(Constants.SYS_MESSAGE, "修改密码失败！");
         }
         request.getRequestDispatcher("pwdmodify.jsp").forward(request, response);
+    }
+
+    // 验证旧密码,session中有用户的密码
+    public void pwdModify(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        // 从Session中取ID
+        Object o = req.getSession().getAttribute(Constants.USER_SESSION);
+        String oldpassword = req.getParameter("oldpassword");
+        Map<String, String> resultMap = new HashMap<String, String>();
+
+        // session过期
+        if(null == o ){
+            resultMap.put("result", "sessionerror");
+        }else if(StringUtils.isNullOrEmpty(oldpassword)){
+            // 旧密码输入为空
+            resultMap.put("result", "error");
+        }else{
+            String sessionPwd = ((User)o).getUserPassword();
+            if(oldpassword.equals(sessionPwd)){
+                resultMap.put("result", "true");
+            }else{
+                // 旧密码输入不正确
+                resultMap.put("result", "false");
+            }
+        }
+
+        resp.setContentType("application/json");
+        PrintWriter outWriter = resp.getWriter();
+        // JSONArray 阿里巴巴的JSON工具类
+        outWriter.write(JSONArray.toJSONString(resultMap));
+        outWriter.flush();
+        outWriter.close();
+
     }
 }
