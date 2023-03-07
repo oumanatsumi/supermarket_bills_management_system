c6439c284cb90dbe9c150768322402da03465032
oumanatsumi <oumanatsumi@126.com>
2023-03-07 09:06:04 +0800

    实现用户查询功能servlet部分

--- src/main/java/com/oumanatsumi/servlet/user/UserServlet.java
+++ src/main/java/com/oumanatsumi/servlet/user/UserServlet.java
@@ -2,10 +2,14 @@ package com.oumanatsumi.servlet.user;
 
 import com.alibaba.fastjson.JSONArray;
 import com.mysql.jdbc.StringUtils;
+import com.oumanatsumi.pojo.Role;
 import com.oumanatsumi.pojo.User;
+import com.oumanatsumi.service.user.RoleService;
+import com.oumanatsumi.service.user.RoleServiceImpl;
 import com.oumanatsumi.service.user.UserService;
 import com.oumanatsumi.service.user.UserServiceImpl;
 import com.oumanatsumi.utils.Constants;
+import com.oumanatsumi.utils.PageSupport;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserServlet extends HttpServlet {
@@ -32,6 +37,8 @@ public class UserServlet extends HttpServlet {
             this.updatePwd(request, response);
         }else if(method.equals("pwdmodify") && method!=null){
             this.pwdModify(request,response);
+        }else if(method != null && method.equals("query")) {
+            this.query(request,response);
         }
     }
 
@@ -92,5 +99,72 @@ public class UserServlet extends HttpServlet {
         outWriter.flush();
         outWriter.close();
 
+    }
+
+    // 用户管理
+    public void query(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // 查询用户列表
+        String queryUserName = request.getParameter("queryname");
+        String temp = request.getParameter("queryUserRole");
+        String pageIndex = request.getParameter("pageIndex");
+        int queryUserRole = 0;
+        UserService userService = new UserServiceImpl();
+        List<User> userList = null;
+
+        // 设置页面容量
+        int pageSize = Constants.pageSize;
+
+        // 当前页码
+        int currentPageNo = 1;
+
+        System.out.println("queryUserName servlet-------->"+queryUserName);
+        System.out.println("queryUserRole servlet-------->"+queryUserRole);
+        System.out.println("query pageIndex----------> " + pageIndex);
+
+        if(queryUserName == null){
+            queryUserName = "";
+        }
+        if(temp != null && !temp.equals("")){
+            queryUserRole = Integer.parseInt(temp);
+        }
+
+        if(pageIndex != null){
+            try{
+                currentPageNo = Integer.valueOf(pageIndex);
+            }catch(NumberFormatException e){
+                response.sendRedirect("error.jsp");
+            }
+        }
+        // 总数量（表）
+        int totalCount	= userService.getUserCount(queryUserName,queryUserRole);
+        // 总页数
+        PageSupport pages=new PageSupport();
+        pages.setCurrentPageNo(currentPageNo);
+        pages.setPageSize(pageSize);
+        pages.setTotalCount(totalCount);
+
+        int totalPageCount = pages.getTotalPageCount();
+
+        // 控制首页和尾页
+        if(currentPageNo < 1){
+            currentPageNo = 1;
+        }else if(currentPageNo > totalPageCount){
+            currentPageNo = totalPageCount;
+        }
+
+
+        userList = userService.getUserList(queryUserName,queryUserRole,currentPageNo, pageSize);
+        request.setAttribute("userList", userList);
+        List<Role> roleList = null;
+        RoleService roleService = new RoleServiceImpl();
+        roleList = roleService.getRoleList();
+        request.setAttribute("roleList", roleList);
+        request.setAttribute("queryUserName", queryUserName);
+        request.setAttribute("queryUserRole", queryUserRole);
+        request.setAttribute("totalPageCount", totalPageCount);
+        request.setAttribute("totalCount", totalCount);
+        request.setAttribute("currentPageNo", currentPageNo);
+        request.getRequestDispatcher("userlist.jsp").forward(request, response);
     }
 }
--- src/main/java/com/oumanatsumi/utils/Constants.java
+++ src/main/java/com/oumanatsumi/utils/Constants.java
@@ -3,4 +3,5 @@ package com.oumanatsumi.utils;
 public class Constants {
     public final static String USER_SESSION = "userSession";
     public final static String SYS_MESSAGE = "message";
+    public final static int pageSize = 5;
 }
